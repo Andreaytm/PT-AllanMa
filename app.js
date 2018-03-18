5d38ab40acd9e62b281e1e2d48853297756eb030
Andreaytm <andrea.ma@hotmail.co.uk>
2018-03-18 11:23:29 +0000

    added app.js routing

---
+++ app.js
@@ -0,0 +1,32 @@
+angular.module("PTSite", ["ngRoute", "RouteControllers"]); 
+
+angular.module("PTSite").config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true);
+   
+	$routeProvider
+	.when("/", {
+		templateUrl: "templates/home.html",
+		controller: "HomeController"
+	})
+	.when("/aboutme", {
+		templateUrl: "templates/aboutme.html",
+		controller: "AboutController"
+	})
+	.when("/yourworkout", {
+		templateUrl: "templates/yourworkout.html",
+		controller: "WorkoutController"
+	})
+	.when("/faq", {
+		templateUrl: "templates/faq.html",
+		controller: "FAQController"
+	})
+	.when("/contactme", {
+		templateUrl: "templates/contactme.html",
+		controller: "ContactController"
+	})
+	.when("/contactme/form", {
+		templateUrl: "templates/contactmeform.html",
+		controller: "SubmitController"
+	});
+});
+
