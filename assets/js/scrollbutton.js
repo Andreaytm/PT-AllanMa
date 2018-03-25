e2a49b486cd178efdfd6ef38ee0d8572188082b6
Andreaytm <andrea.ma@hotmail.co.uk>
2018-03-25 19:17:20 +0100

    updated js syntax

--- assets/js/scrollbutton.js
+++ assets/js/scrollbutton.js
@@ -1,15 +1,15 @@
-window.onscroll = function() {scrollFunction()};/* taken from https://www.w3schools.com/ */
+window.onscroll = function() {scroll();};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-        document.getElementById("toTop").style.display = "block";
-    } else {
-        document.getElementById("toTop").style.display = "none";
-    }
-};
+function scroll() {
+	if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+		document.getElementById("toTop").style.display = "block";
+	} else {
+		document.getElementById("toTop").style.display = "none";
+	}
+}
 
-// When the user clicks on the button, scroll to the top of the document
+// When the user clicks on button scroll to the top of the page
 function GoToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0; 
 }
