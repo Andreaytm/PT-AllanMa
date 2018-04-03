6472e5afed0c7bd7f2b189acf25d26acb6b87cce
Andreaytm <andrea.ma@hotmail.co.uk>
2018-04-04 00:22:52 +0100

    added library

---
+++ assets/js/datepicker.js
@@ -0,0 +1,14 @@
+ $(document).ready(function() {
+ 	$(window).on("shown.bs.modal", function() { 
+ 		$("#datetimepicker5").datetimepicker({
+ 			format: "ddd DD MMM YYYY HH:mm",
+ 			defaultDate: moment(),
+ 			minDate: moment(),
+ 			maxDate: moment().add(365, "days"),
+ 			daysOfWeekDisabled: [1],
+ 			stepping: 30,
+ 			disabledHours: [00,01,02,03,04,05,06,07,08,19,20,21,22,23],
+ 		});
+ 	});
+ });
+
