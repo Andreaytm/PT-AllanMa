6d663175403545429c0b8244a2ca2b103cca1dda
Andreaytm <andrea.ma@hotmail.co.uk>
2018-04-14 15:09:37 +0100

    added audio js

---
+++ assets/js/audio.js
@@ -0,0 +1,19 @@
+/*taken from Stack Overflow -Nathan Long
+https://stackoverflow.com/questions/20719550/play-one-html-audio-element-at-a-time
+https://stackoverflow.com/users/4376/nathan-long*/
+
+function oneAudio(container) {
+  container.addEventListener("play", function(event) {
+  audio_player = container.getElement("audio")
+    for(i=0; i < audio_player.length; i++) {
+      audio_player = audio_player[i];
+      if (audio_player !== event.target) {
+        audio_player.pause();
+      }
+    }
+  }, true);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  oneAudio(document.body);
+});
