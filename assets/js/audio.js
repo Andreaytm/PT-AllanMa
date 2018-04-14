542b7310afe106f5a50999cadd48f6c824a4d805
Andreaytm <andrea.ma@hotmail.co.uk>
2018-04-14 15:19:36 +0100

    updated js

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -2,18 +2,19 @@
 https://stackoverflow.com/questions/20719550/play-one-html-audio-element-at-a-time
 https://stackoverflow.com/users/4376/nathan-long*/
 
-function oneAudio(container) {
+function onlyPlayOneIn(container) {
   container.addEventListener("play", function(event) {
-  audio_player = container.getElementsByTagName("audio")
-    for(i=0; i < audio_player.length; i++) {
-      audio_player = audio_player[i];
-      if (audio_player !== event.target) {
-        audio_player.pause();
+  audio_elements = container.getElementsByTagName("audio")
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element !== event.target) {
+        audio_element.pause();
       }
     }
   }, true);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  oneAudio(document.body);
+  onlyPlayOneIn(document.body);
 });
+
